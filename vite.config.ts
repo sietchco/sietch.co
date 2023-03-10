1c85ff7efdb6a3429ebdc25c0ee00cd63943ffca
Paul Diaz <paulrrdiaz@gmail.com>
2023-03-10 01:49:01 -0500

    First commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsConfigPaths(), eslintPlugin()],
+  plugins: [react(), tsConfigPaths(), eslintPlugin({ eslintOptions: { cache: false } })],
 })
