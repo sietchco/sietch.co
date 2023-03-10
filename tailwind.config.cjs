fde81a0515bc518a92ba725a2cae81992d31b0ee
Paul Diaz <paulrrdiaz@gmail.com>
2023-03-10 02:26:44 -0500

    Add TailwindCSS

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'ui-sand': '#F5E9CF',
+        'ui-purple': '#4D455D',
+        'ui-red': '#E96479',
+        'ui-green': '#7DB9B6',
+        'ui-gray': '#3D3C42',
+        'ui-black': '#1A120B',
+        'ui-white': '#FEFBF6',
+      },
+    },
+  },
+  plugins: [],
+}
+
+// Main palette: https://colorhunt.co/palette/4d455de96479f5e9cf7db9b6
+// black: https://colorhunt.co/palette/1a120b3c2a21d5cea3e5e5cb
+// gray / white: https://colorhunt.co/palette/fefbf6a6d1e67f52833d3c42
