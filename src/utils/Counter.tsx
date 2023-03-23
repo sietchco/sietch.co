ea8e439a0930b67885266e51860b7ac01f050567
LizardoCt <lizardo_93@hotmail.com>
2023-03-23 17:58:53 -0500

    Updated to TS

--- src/utils/Counter.tsx
+++ src/utils/Counter.tsx
@@ -1,21 +1,27 @@
 import { animate } from "framer-motion";
 import { useEffect, useRef } from "react";
 
-function Counter({ from, to }) {
-  const nodeRef = useRef()
+type CounterProps = {
+  from: number
+  to: number
+}
+
+const Counter = ({ from, to } : CounterProps) => {
+  const nodeRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
-    const node = nodeRef.current;
-
-    const controls = animate(from, to, {
-      duration: 2.5,
-      onUpdate(value) {
-        node.textContent = value.toFixed();
-      }
-    });
-
-    return () => controls.stop();
-  }, [from, to]);
+    const node = nodeRef.current
+    
+    if (node) {
+      const controls = animate(from, to, {
+        duration: 2.5,
+        onUpdate(value) {
+          node.textContent = value.toFixed()
+        }
+      });
+      return () => controls.stop()
+    }
+  }, [from, to])
 
   return(
       <p ref={nodeRef}></p>
